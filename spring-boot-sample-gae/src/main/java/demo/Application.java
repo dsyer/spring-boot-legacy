37549b99e7af1e92f16b5a12e5ff8767271ee893
willedwards <will.edwards@me.com>
2016-09-18 16:02:32 +0100

    added relative path in pom, added some basic logging, updated README

--- spring-boot-sample-gae/src/main/java/demo/Application.java
+++ spring-boot-sample-gae/src/main/java/demo/Application.java
@@ -2,15 +2,11 @@ package demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @RestController
 public class Application {
 	
