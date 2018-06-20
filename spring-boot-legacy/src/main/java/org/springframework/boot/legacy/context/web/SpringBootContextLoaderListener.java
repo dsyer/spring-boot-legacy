d0533b029c993a44ea9014e70026509409e7938a
Daniel Cruver <ddcruver@gmail.com>
2018-06-20 17:25:08 -0400

    Improve SpringBootContextLoaderListerner to be closer to SpringBootServletInitalizer. Set main class, enabled white label error pages, check that sources are configured etc.

--- spring-boot-legacy/src/main/java/org/springframework/boot/legacy/context/web/SpringBootContextLoaderListener.java
+++ spring-boot-legacy/src/main/java/org/springframework/boot/legacy/context/web/SpringBootContextLoaderListener.java
@@ -17,6 +17,7 @@
 package org.springframework.boot.legacy.context.web;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import javax.servlet.ServletContext;
 
@@ -32,6 +33,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.context.ContextLoaderListener;
@@ -94,37 +98,49 @@ public class SpringBootContextLoaderListener extends ContextLoaderListener {
 		environment.initPropertySources(servletContext, null);
 		builder.environment(environment);
 
+		setMainClass(builder, classNames);
+
 		@SuppressWarnings("unchecked")
 		Class<? extends ConfigurableApplicationContext> contextClass = (Class<? extends ConfigurableApplicationContext>) determineContextClass(servletContext);
 		builder.contextClass(contextClass);
 
-		WebApplicationContext context;
 		ApplicationContext parent = getExistingRootWebApplicationContext(servletContext);
+
 		if (parent != null) {
-			servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, null);
 			this.logger.info("Root context already created (using as parent).");
+			servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, null);
 			builder.initializers(new ParentContextApplicationContextInitializer(parent));
-
-			context = (WebApplicationContext) builder.run();
-		} else {
-			logger.info("No existing root context; will created one.");
-
-			builder.initializers(new ApplicationContextInitializer<GenericWebApplicationContext>() {
-				@Override
-				public void initialize(GenericWebApplicationContext applicationContext) {
-					applicationContext.setServletContext(servletContext);
-				}
-			});
-			context = (WebApplicationContext) builder.run();
-			servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
 		}
+
+		builder.initializers((ApplicationContextInitializer<GenericWebApplicationContext>) applicationContext -> applicationContext.setServletContext(servletContext));
 
 		// Ensure error pages are registered
 		if (this.registerErrorPageFilter) {
 			builder.sources(ErrorPageFilterConfiguration.class);
 		}
 
+		SpringApplication application = builder.build();
+
+		if (application.getAllSources().isEmpty() && AnnotationUtils.findAnnotation(getClass(), Configuration.class) != null) {
+			application.addPrimarySources(Collections.singleton(getClass()));
+		}
+
+		Assert.state(!application.getAllSources().isEmpty(),
+				"No SpringApplication sources have been defined. Either override the "
+						+ "configure method or add an @Configuration annotation");
+
+		WebApplicationContext context = (WebApplicationContext) application.run();
+		servletContext.setAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE, context);
 		return context;
+	}
+
+	private void setMainClass(SpringApplicationBuilder builder, String[] classNames) {
+		try {
+			builder.main(Class.forName(classNames[0]));
+		}
+		catch (ClassNotFoundException e) {
+			this.logger.warn("Could create instance of class " + classNames[0] + " provided by " + CONFIG_LOCATION_PARAM, e);
+		}
 	}
 
 	private void setRegisterErrorPageFilterFromContextParam(ServletContext servletContext) {
